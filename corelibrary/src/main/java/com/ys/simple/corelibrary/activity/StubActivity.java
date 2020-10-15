9a6ca2b2cf6768b1436887a76df10ad70e4cfbc6
yinshan <ys_sarah@163.com>
2020-10-15 17:54:09 +0800

    add stub activity
    Signed-off-by: yinshan <ys_sarah@163.com>

---
+++ corelibrary/src/main/java/com/ys/simple/corelibrary/activity/StubActivity.java
@@ -0,0 +1,16 @@
+package com.ys.simple.corelibrary.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+
+/**
+ * Created by yinshan on 2020/10/14.
+ */
+public class StubActivity extends Activity {
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
--- corelibrary/src/main/java/com/ys/simple/corelibrary/hook/VAInstrumentation.java
+++ corelibrary/src/main/java/com/ys/simple/corelibrary/hook/VAInstrumentation.java
@@ -2,14 +2,20 @@ package com.ys.simple.corelibrary.hook;
 
 import android.app.Instrumentation;
 
+import com.ys.simple.corelibrary.PluginManager;
+
 /**
  * Created by yinshan on 2020/10/14.
  */
 public class VAInstrumentation extends Instrumentation {
 
+  private PluginManager mPluginManager;
   private Instrumentation mBase;
 
-  public VAInstrumentation(Instrumentation base) {
-    mBase = base;
+  public VAInstrumentation(PluginManager pluginManager, Instrumentation base) {
+    this.mPluginManager = pluginManager;
+    this.mBase = base;
   }
+
+
 }
